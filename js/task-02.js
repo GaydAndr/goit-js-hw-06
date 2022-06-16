90cd1a9347e0ecda2453c34261ffb1b57bc348e9
andrii_h <galgayand@gmail.com>
2022-06-16 17:42:08 +0300

    done Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElement = document.querySelector('#ingredients');
+
+const liItems = ingredients.map((element) => {
+  const liElem = document.createElement('li');
+  liElem.classList.add('item');
+  liElem.textContent = `${element}`;
+  // console.log(liElem);
+  return liElem;
+});
+// console.log(liItems);
+
+ulElement.append(...liItems);
+
+// ingredients.forEach((element) => {
+//   const liElement = document.createElement('li');
+//   liElement.textContent = `${element}`;
+//   liElement.classList.add('item');
+//   console.log(liElement);
+//   ulElement.appendChild(liElement);
+// });
