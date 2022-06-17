0a0651fbd4ac6fe5ce433a4b67ac7e89c75e1e68
andrii_h <galgayand@gmail.com>
2022-06-17 20:22:42 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,21 @@
 const inputValue = document.querySelector('#name-input');
 const outputValue = document.querySelector('#name-output');
 
-inputValue.addEventListener('input', () => {
-  // console.dir(inputValue.value);
+inputValue.addEventListener('input', insertsValue);
+
+function insertsValue() {
   outputValue.textContent = inputValue.value;
   if (inputValue.value === '') {
     outputValue.textContent = 'Anonymous';
   }
-});
+}
+
+//-------------------
+
+// inputValue.addEventListener('input', () => {
+//   // console.dir(inputValue.value);
+//   outputValue.textContent = inputValue.value;
+//   if (inputValue.value === '') {
+//     outputValue.textContent = 'Anonymous';
+//   }
+// });
