56a85e8fd0d5520a0165126140dcf83e94e58906
andrii_h <galgayand@gmail.com>
2022-06-18 00:19:17 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+const userData = {};
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Будь-ласка заповніть всі поля!');
+  }
+
+  userData.email = email.value;
+  userData.password = password.value;
+
+  console.log(userData);
+
+  event.currentTarget.reset();
+}
