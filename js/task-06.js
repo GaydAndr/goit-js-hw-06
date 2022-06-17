4eec9aa4a49dff7746c840d63d8c6857f518e123
andrii_h <galgayand@gmail.com>
2022-06-18 00:19:06 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,36 @@
 const inputValue = document.querySelector('#validation-input');
 const validValue = Number(inputValue.dataset.length);
 
-inputValue.addEventListener('blur', () => {
-  console.dir(inputValue.value.length);
-  if (validValue === inputValue.value.length) {
-    if (inputValue.classList.contains('invalid')) {
-      inputValue.classList.remove('invalid');
+inputValue.addEventListener('blur', (event) => {
+  console.log(event.target.value.length);
+  if (validValue === event.target.value.length) {
+    if (event.target.classList.contains('invalid')) {
+      event.target.classList.remove('invalid');
     }
-    inputValue.classList.add('valid');
+    event.target.classList.add('valid');
     return;
   }
 
-  if (inputValue.classList.contains('valid')) {
-    inputValue.classList.remove('valid');
+  if (event.target.classList.contains('valid')) {
+    event.target.classList.remove('valid');
   }
-  inputValue.classList.add('invalid');
+  event.target.classList.add('invalid');
 });
+
+//?-- Як правильніше -- event.target - or - inputValue ---//
+
+// inputValue.addEventListener('blur', (event) => {
+//   console.log(inputValue.value.length);
+//   if (validValue === inputValue.value.length) {
+//     if (inputValue.classList.contains('invalid')) {
+//       inputValue.classList.remove('invalid');
+//     }
+//     inputValue.classList.add('valid');
+//     return;
+//   }
+
+//   if (inputValue.classList.contains('valid')) {
+//     inputValue.classList.remove('valid');
+//   }
+//   inputValue.classList.add('invalid');
+// });
