389c91f4f1977cf072c286866f41a2c34ac3d2bc
andrii_h <galgayand@gmail.com>
2022-06-18 00:34:45 +0300

    done task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  document.body.style.background = color;
+  span.textContent = color;
+});
+
+// function changeBodyColor() {
+//   const color = getRandomHexColor();
+//   document.body.style.background = getRandomHexColor();
+//   btn.previousElementSibling.firstElementChild.textContent = color;
+// }
